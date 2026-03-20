493572cef6148a7c40d46de4924f6cd5a072e4ef
Chapman, Anne <annie123c@gmail.com>
2026-03-20 13:21:16 -0700

    Code Written

--- Assignment3.1.cpp
+++ Assignment3.1.cpp
@@ -6,5 +6,30 @@ using namespace std;
 int main()
 {
 
-return 0;
+    const int NUM_STORES = 5;
+    int sales[NUM_STORES];
+
+    for (int i = 0; i < NUM_STORES; i++)
+    {
+        cout << "Enter sales for store " << i + 1 << ": ";
+        cin >> sales[i];
+    }
+
+    cout << "\nSALES BAR CHART\n";
+    cout << "(Each * = $100)" << endl;
+
+    for (int i = 0; i < NUM_STORES; i++)
+    {
+        cout << "Store " << i + 1 << ": ";
+        
+        int stars = sales[i] / 100;
+
+        for (int j = 0; j < stars; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+        
+    return 0;
 }
