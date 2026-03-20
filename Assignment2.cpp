cb24123dd3cea1bb619f3f7357182e9ad55ae56e
Chapman, Anne <annie123c@gmail.com>
2026-03-20 13:33:00 -0700

    Code Written

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -5,5 +5,24 @@ int main() {
 
    /* Type your code here. */
 
+   int start;
+   int end;
+   cout << "Enter the starting value: ";
+   cin >> start;
+   cout << "Enter the ending value: ";
+   cin >> end;
+
+   if (end < start)
+   {
+      cout << "The ending integer cannot be less than the starting integer." << endl;
+   }
+   else
+   {
+      for (int i = start; i <= end; i += 5)
+      {
+         cout << i << " ";
+      }
+   }
+
    return 0;
 }
